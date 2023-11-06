c991b84cb2ef113dc32e58030bfa43627195c5ea
Marvin <bugsbunny@gmail.com>
2023-11-06 22:19:38 +0000

    using javascript to dynamically control dropdown menus

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let serviceEl = document.getElementById('services-menu')
+console.log(serviceEl)
+let serviceList = document.getElementById('services-list')
+console.log(serviceList)
+
+
+
+serviceEl.addEventListener('mouseover', (event) =>{
+  event.preventDefault()
+  serviceList.classList.add('transform-menu')
+ })
+
+ serviceList.addEventListener('mouseover', (event) =>{
+  serviceList.classList.add('transform-menu')
+ })
+
+ serviceList.addEventListener('mouseout', (event) =>{
+  serviceList.classList.remove('transform-menu')
+ })
+
+ serviceEl.addEventListener('mouseout', (event) =>{
+  serviceList.classList.remove('transform-menu')
+ })
